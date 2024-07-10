ee3e5dacb50a88b75952b2c7d9ae4668664ea6c6
Asim Sumair <103741875+AsimSumair@users.noreply.github.com>
2024-07-10 10:48:39 +0530

    Update main.py
    Change distances

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ load_dotenv()
 connection_string = os.getenv("MONGODB_CONNECTION_STRING")
 
 # Define the allowed location coordinates (latitude, longitude)
-ALLOWED_LOCATION = (34.1011, 74.8090)  # Example coordinates
-MAX_DISTANCE_KM = 10.0  # Maximum allowed distance in kilometers
+ALLOWED_LOCATION = (34.1008979, 74.8099825)  # Example coordinates
+MAX_DISTANCE_KM = 0.5  # Maximum allowed distance in kilometers
 
 # Connect to MongoDB
 client = pymongo.MongoClient(connection_string)
